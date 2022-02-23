4926425b928b8c4e0ca0d45cc9a0848095a9e220
Jonatan Alestig <93116520+Pipsqueack@users.noreply.github.com>
2022-02-23 13:43:28 +0100

    Lektion 2022-02-23
    Påbörjade kodning av huvudsidan och lade till:
    Header med titel och knappar till de andra sidorna
    Burger system med knappar till de andra sidorna
    Main container som kommer innehålla huvudbilder, titel/logga osv

---
+++ js/script.js
@@ -0,0 +1,9 @@
+const nav = document.getElementById('nav_links')
+const burger = document.getElementById('burger')
+
+function toggleMenu() {
+    nav.classList.toggle('nav_active')
+    burger.classList.toggle('crossed_line')
+}
+
+burger.addEventListener('click', toggleMenu)
