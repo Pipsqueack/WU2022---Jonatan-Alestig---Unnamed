18defa92a5cee470fa19e67c79bc5b9cb40f9cf7
Jonatan Alestig <93116520+Pipsqueack@users.noreply.github.com>
2022-04-01 11:28:17 +0200

    Big ändringar

--- js/script.js
+++ js/script.js
@@ -1,9 +1,19 @@
 const nav = document.getElementById('nav_links')
 const burger = document.getElementById('burger')
+const bookUs = document.getElementById('bookUs')
 
 function toggleMenu() {
     nav.classList.toggle('nav_active')
     burger.classList.toggle('crossed_line')
 }
 
+function scrollToBottom() {
+    window.scrollTo({
+        top: document.body.scrollHeight,
+        behavior: 'smooth',
+      });
+}
+
 burger.addEventListener('click', toggleMenu)
+bookUs.addEventListener('click', scrollToBottom)
+
